a2811ead674ebdfc3895fe7cfbeb6e0379ba7da7
1489444965@qq.com <1489444965@qq.com>
2021-12-15 23:56:25 +0800

    添加table api demo

---
+++ flink-start/src/main/java/com/betop/basic/table/TableApiDemo.java
@@ -0,0 +1,46 @@
+package com.betop.basic.table;
+
+import com.betop.basic.entity.WaterSensor;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+import static org.apache.flink.table.api.Expressions.$;
+
+/**
+ * @author long
+ * @description:
+ * @date 2021/12/15 23:53
+ */
+public class TableApiDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<WaterSensor> waterSensorStream =
+                env.fromElements(new WaterSensor("sensor_1", 1000L, 10),
+                        new WaterSensor("sensor_1", 2000L, 20),
+                        new WaterSensor("sensor_2", 3000L, 30),
+                        new WaterSensor("sensor_1", 4000L, 40),
+                        new WaterSensor("sensor_1", 5000L, 50),
+                        new WaterSensor("sensor_2", 6000L, 60));
+
+        // 1. 创建表的执行环境
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+        // 2. 创建表: 将流转换成动态表. 表的字段名从pojo的属性名自动抽取
+        Table table = tableEnv.fromDataStream(waterSensorStream);
+        // 3. 对动态表进行查询
+        Table resultTable = table
+                .where($("id").isEqual("sensor_1"))
+                .select($("id"), $("ts"), $("vc"));
+        // 4. 把动态表转换成流
+        DataStream<Row> resultStream = tableEnv.toAppendStream(resultTable, Row.class);
+        resultStream.print();
+
+        env.execute();
+    }
+
+}
